77ea102aff8a626bd2abe80585f832ec22fcfcc5
Mario Mouro <mario.mouro@live.com>
2020-06-24 20:53:39 -0300

    Mensagem explicativa

--- src/Applications/Program.java
+++ src/Applications/Program.java
@@ -3,7 +3,7 @@ package Applications;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Alguns exercícios do curso \"Java completo\" do Prof. Nélio Alves na Udemy.");
 
 	}
 
