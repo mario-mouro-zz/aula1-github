29d6bcbfd08965e5bd492d56701407907fed81f7
Mario Mouro <mario.mouro@live.com>
2020-06-24 21:24:52 -0300

    Mensagem de boa noite

--- src/Applications/Program.java
+++ src/Applications/Program.java
@@ -4,7 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Alguns exercícios do curso \"Java completo\" do Prof. Nélio Alves na Udemy.");
-		System.out.println("Essa frase será apagada!");
+		System.out.println("Bom dia!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
 
 }
