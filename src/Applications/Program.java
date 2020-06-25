8e190f1ff9cbde842a416b9c9cd3890c4b9f7385
Mario Mouro <mario.mouro@live.com>
2020-06-24 21:01:17 -0300

    Acrescentada uma frase que será apagada

--- src/Applications/Program.java
+++ src/Applications/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Alguns exercícios do curso \"Java completo\" do Prof. Nélio Alves na Udemy.");
-
+		System.out.println("Essa frase será apagada!");
 	}
 
 }
